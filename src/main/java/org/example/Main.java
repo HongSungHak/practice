0936f5674033f2e416dd955ac64a1243b90728b6
hong <hong@kakao.com>
2022-12-14 23:56:29 +0900

    practice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] Iarr = {100, 95, 90, 85, 80};
+        System.out.println("Iarr = " + Arrays.toString(Iarr));
     }
 }
