60fc2f9bc532c60b9080df093ed5587c9c70ad3f
hong <hong@kakao.com>
2022-12-22 17:18:45 +0900

    StringBuffer클래스

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,26 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String animals = "dog, cat, bear";
-        String[] arr = animals.split(",");
-        String str = String.join("-", arr);
-        System.out.println("str = " + str); //join메서드를 사용하여 나누기
+        /**
+         * StringBuffer의 비교
+         * StringBuffer는 equlas()가 오버라이딩 되어 있지 않다.(주소비교)
+         * StringBuffer을 String으로 변환 후 equals()로 비교해야 한다.
+         */
+        StringBuffer sb = new StringBuffer("abc");
+        StringBuffer sb2 = new StringBuffer("abc");
+        String s = sb.toString();
+        String s2 = sb2.toString();
+        System.out.println(sb == sb2);
+        System.out.println(sb.equals(sb2));
+        System.out.println(s.equals(s2));
 
-        //숫자를 문자열로 바꾸는 방법
-        int i = 100;
-        String str1 = i + ""; //속도가 느림
-        String str2 = String.valueOf(i); //valueOf 메서드를 사용해서 속도가 빠름
 
-        //문자열을 숫자로 바꾸는 방법
-        int i2 = Integer.parseInt("100"); //valueOf로 통일시킴
-        int i3 = Integer.valueOf("100");
-        Integer i4 = Integer.valueOf("100"); //valueOf은 참조형으로 반환함
+//        sb.delete() 삭제
+//        sb.insert()삽입
+//        StringBuffer sb2 = sb.append("ZZ");
+
+
+
+
     }
 }
