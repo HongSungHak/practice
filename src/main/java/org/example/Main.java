9adf7834d9f1ae88397675a352d9d797752130f9
hong <qmffndnjzm@naver.com>
2023-08-08 15:53:26 +0900

    linkedList impl

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,11 @@ package org.example;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        List<Integer> list = new LinkedList<>();
     }
 }
--- src/main/java/org/example/StringBuffer/StringBufferClass.java
+++ src/main/java/org/example/StringBuffer/StringBufferClass.java
@@ -1,11 +1,22 @@
 package org.example.StringBuffer;
 
-import java.io.File;
-import java.util.Stack;
+
+import java.util.List;
+import java.util.ListIterator;
+
+
+
 
 public class StringBufferClass {
     public static void main(String[] args) {
 
+        int sum = 0;
 
+        List<Integer> integers = List.of(1, 2, 3, 4, 5);
+        ListIterator<Integer> integerListIterator = integers.listIterator();
+        while (integerListIterator.hasNext()) {
+            sum += integerListIterator.next();
+            System.out.println(sum);
+        }
     }
 }
