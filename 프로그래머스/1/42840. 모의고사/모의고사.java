ec900b64c0af6b2b19e35fbd1379101a870fa70f
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2023-10-26 12:48:37 +0900

    [level 1] Title: 모의고사, Time: 2.59 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,7 +3,7 @@ class Solution {
     public int[] solution(int[] answers) {
         int[] student1 = {1, 2, 3, 4, 5};
         int[] student2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5,};
+        int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         int[] count = new int[3];
         for (int i = 0; i < answers.length; i++) {
             if (student1[i%5] == answers[i]) count[0]++;
