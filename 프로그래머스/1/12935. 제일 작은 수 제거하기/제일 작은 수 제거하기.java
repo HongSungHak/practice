b3798d8d48e44f06228c15ee3b008b6035772a73
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2023-11-01 16:01:06 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.29 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,9 +1,8 @@
 import java.util.*;
-import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int[] arr) {
         if (arr.length <= 1) return new int[]{-1};
         int min = Arrays.stream(arr).min().getAsInt();
-        return IntStream.range(0, arr.length).map(i -> arr[i]).filter(i -> i != min).toArray();
+        return Arrays.stream(arr).filter(i -> i != min).toArray();
     }
 }
