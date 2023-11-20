8db19f9303df8d50420ea7a128ec77ac0680aa36
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2023-11-20 13:45:56 +0900

    [Bronze V] Title: A+B - 8, Time: 288 ms, Memory: 18948 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int sum = a + b;
+            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + sum);
+        }
+    }
+}
