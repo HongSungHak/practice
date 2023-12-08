bba2066be92ae4642a452861b855c50b3cc974b1
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2023-12-09 00:01:30 +0900

    [level unrated] Title: 배열 조각하기, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -1,13 +1,16 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[] query) {
+        int start = 0;
+        int end = arr.length - 1;
         for (int i = 0; i < query.length; i++) {
             if (i % 2 == 0) {
-                arr = Arrays.copyOfRange(arr, 0, query[i] + 1);
+                end = start + query[i] - 1;
             } else {
-                arr = Arrays.copyOfRange(arr, query[i], arr.length);
+                start += query[i];
             }
         }
-        return arr;
+
+        return Arrays.copyOfRange(arr, start, end + 2);
     }
 }
