f664b0166899b6fa684b3ca2f31895d975444e75
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2024-04-12 23:58:03 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    
+    string id;
+    cin >> id;
+    
+    id += "??!";
+    cout << id;
+ 
+    return 0;
+}
