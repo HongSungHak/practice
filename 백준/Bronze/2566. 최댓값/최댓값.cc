07e8e7ec0abb1658aacb505eddb8c9ad55e29366
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2024-05-18 18:04:10 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.cc
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
+
 const int COL = 9;
 const int ROW = 9;
 int arr[COL][ROW];
+
 int main()
 {
     int max = 0;
@@ -12,23 +14,19 @@ int main()
         for (int j = 0; j < ROW; j++)
         {
             cin >> arr[i][j];
-        }
-    }
-    for (int i = 0; i < COL; i++)
-    {
-        for (int j = 0; j < ROW; j++)
-        {
-            if (arr[i][j] > max) {
+            if (arr[i][j] > max)
+            {
                 max = arr[i][j];
                 col = i + 1;
                 row = j + 1;
-            } else if(max == 0)
+            } else if (max == 0)
             {
                 col = 1;
                 row = 1;
             }
         }
     }
+    
     cout << max << '\n';
     cout << col << " " << row;
 }
