9b278ee061a8b91ae972bbe9d6a0760669b58bb8
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2024-07-30 20:28:34 +0900

    [level 0] Title: 순서쌍의 개수, Time: 19.11 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,6 @@
+import java.util.stream.IntStream;
+class Solution {
+    public int solution(int n) {
+        return (int) IntStream.rangeClosed(1, n).filter(num -> n % num == 0).count();
+    }
+}
