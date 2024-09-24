cbe98975d8a0199ea479caac743d894ff2866a9a
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2024-09-24 19:34:18 +0900

    [level 0] Title: k의 개수, Time: 0.58 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int i, int j, int k) {
+    int answer = 0;
+
+    for (int s = i; s <= j; s++) {
+        int num = s;
+        while (num > 0) {
+            if (num % 10 == k) answer++;
+            num /= 10;
+        }
+    }
+
+    return answer;
+}
