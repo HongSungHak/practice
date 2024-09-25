9978370e01ea29247c56d7fab85aa4d7383a2c62
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2024-09-25 22:00:48 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 30.23 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,14 +1,10 @@
-import java.util.*;
+import java.util.stream.IntStream;
 
 public class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> list = new ArrayList<>();
-        list.add(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i - 1] != arr[i]) {
-                list.add(arr[i]);
-            }
-        }
-        return list.stream().mapToInt(i -> i).toArray();
+        return IntStream.range(0, arr.length)
+            .filter(i -> i == 0 || arr[i - 1] != arr[i])
+            .map(i -> arr[i])
+            .toArray();
     }
 }
