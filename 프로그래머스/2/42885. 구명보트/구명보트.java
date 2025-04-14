4dc2cb751dff8a7457aa99052b4135a4de1e9fda
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2025-04-14 21:25:13 +0900

    [level 2] Title: 구명보트, Time: 13.43 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,22 +1,17 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] people, int limit) {
-        //Arrays.stream(people).boxed().sorted().collect(Collections.toList()) 사용시 시간초과남
-        List<Integer> list = new ArrayList<>();
-        for (int i : people) list.add(i);
-        Collections.sort(list);
-
-        ArrayDeque<Integer> dq = new ArrayDeque<>(50505);
-        for(int x : list) dq.add(x);
-
-        int ans = 0;
-        while(dq.isEmpty() == false) {
-            int weight = dq.pollLast();
-            if(dq.isEmpty() == false && weight + dq.peekFirst() <= limit) { dq.pollFirst(); }
-            ans++;
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int count = 0;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            count++;
         }
-
-        return ans;
+        return count;
     }
 }
