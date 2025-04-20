cc32b5b318d7bfda85421b30175a078742e0e3a2
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2025-04-20 15:56:25 +0900

    [Bronze II] Title: 피보나치 수, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    static int[] memo;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        memo = new int[num + 1];
-        for (int i = 0; i <= num; i++) {
-            memo[i] = -1;
+        
+        if (num <= 1) {
+            System.out.println(num);
+            return;
         }
-        int result = fibonacci(num);
-        System.out.println(result);
-    }
-    private static int fibonacci(int num) {
-        if (num <= 1) return num;
-        if (memo[num] != -1) return memo[num];
-        memo[num] = fibonacci(num - 1) + fibonacci(num - 2);
-        return memo[num];
+
+        int[] dp = new int[num + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= num; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[num]);
     }
 }
