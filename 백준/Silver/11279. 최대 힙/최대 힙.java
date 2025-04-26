9962d4bbde775e3c9bacb7e8e679e67f35b62848
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2025-04-26 12:12:59 +0900

    [Silver II] Title: 최대 힙, Time: 788 ms, Memory: 33184 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (pq.isEmpty() && num == 0) {
+                System.out.println(0);
+            } else if (num != 0) {
+                pq.add(num);
+            } else {
+                System.out.println(pq.poll());
+            }
+        }
+
+    }
+}
