a9d5d6d799a521216b4fff4b08314768c8e19da6
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2025-04-28 22:52:13 +0900

    [level 0] Title: 두 수의 합, Time: 428.76 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,7 @@
+import java.math.BigInteger;
+class Solution {
+    public String solution(String a, String b) {
+        BigInteger answer = new BigInteger(a).add(new BigInteger(b));
+        return String.valueOf(answer);
+    }
+}
