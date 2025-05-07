8a15251e28b54df891a7df8ec46fc2c8bbebdeae
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2025-05-07 21:36:47 +0900

    [Silver III] Title: N번째 큰 수, Time: 712 ms, Memory: 212528 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,20 +1,28 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-        for (int i = 0; i < N * N; i++) {
-            int num = sc.nextInt();
-            minHeap.add(num);
-            if (minHeap.size() > N) {
-                minHeap.poll(); 
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            for (int j = 0; j < N; j++) {
+                int num = Integer.parseInt(st.nextToken());
+
+                if (minHeap.size() < N) {
+                    minHeap.offer(num);
+                } else if (num > minHeap.peek()) {
+                    minHeap.poll();
+                    minHeap.offer(num);
+                }
             }
         }
 
-        System.out.println(minHeap.peek()); 
+        System.out.println(minHeap.peek());
     }
 }
