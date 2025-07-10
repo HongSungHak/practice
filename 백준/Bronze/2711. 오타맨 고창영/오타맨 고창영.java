378466aafec65350c068c2e5d32d6705bc6e6ac1
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2025-07-10 22:48:38 +0900

    [Bronze II] Title: 오타맨 고창영, Time: 96 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/2711. 오타맨 고창영/오타맨 고창영.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int index = Integer.parseInt(st.nextToken());
+            String word = st.nextToken();
+            for (int j = 0; j < word.length(); j++) {
+                if (j + 1 == index) continue;
+                sb.append(word.charAt(j));
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
