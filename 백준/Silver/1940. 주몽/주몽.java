e651a909f4023c03e1dc894144bc6e3e4a2ee029
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2025-08-05 20:18:17 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16912 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -4,31 +4,32 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int sources = Integer.parseInt(br.readLine());
-        int needNum = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int[] arr = new int[sources];
-        for (int i = 0; i < sources; i++) {
+        int N = Integer.parseInt(br.readLine());
+        int targetNum = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
+
+        int startIndex = 0;
+        int endIndex = N - 1;
         int count = 0;
-        int start = 0;
-        int end = sources - 1;
-        while (start < end) {
-            if (arr[start] + arr[end] < needNum) {
-                start++;
-            } else if (arr[start] + arr[end] > needNum) {
-                end--;
-            } else {
+        while (startIndex < endIndex) {
+            if (arr[startIndex] + arr[endIndex] == targetNum) {
                 count++;
-                start++;
-                end--;
+                startIndex++;
+                endIndex--;
+            } else if (targetNum > arr[startIndex] + arr[endIndex]) {
+                startIndex++;
+            } else {
+                endIndex--;
             }
-            if(start > end) break;
         }
         System.out.println(count);
     }
