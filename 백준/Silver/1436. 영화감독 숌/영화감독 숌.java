98d7c71b96176603487676890af4d72e066f687b
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2025-08-20 21:07:20 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 85204 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int movieNumber = 666;
+        int count = 0;
+
+        while (true) {
+            if (String.valueOf(movieNumber).contains("666")) {
+                count++;
+            }
+
+            if (count == N) {
+                System.out.println(movieNumber);
+                break;
+            }
+
+            movieNumber++;
+        }
+    }
+}
