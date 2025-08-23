e2648e008a7dde9905e56a80dffe9ced10896acb
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2025-08-23 22:51:02 +0900

    [Silver IV] Title: 수 찾기, Time: 596 ms, Memory: 41924 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,46 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[] A;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
-        A = new int[N];
-        for(int i = 0; i < N; i++)
-            A[i] = sc.nextInt();
-
-        Arrays.sort(A);
-
-        M = sc.nextInt();
-
-        for(int i = 0; i < M; i++){
-            boolean find = false;
-            int target = sc.nextInt();
-            int start = 0;
-            int end = N-1;
-
-            while(start <= end){
-                int mid = (start + end) / 2;
-                int mid_value = A[mid];
-                if(mid_value < target)
-                    start = mid + 1;
-                else if (mid_value > target)
-                    end = mid - 1;
-                else{
-                    find = true;
-                    break;
-                }
-            }
-
-            if(find == true)
-                System.out.println(1);
-            else
-                System.out.println(0);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+
+            if (binarySearch(arr, target)) {
+                sb.append(1).append('\n');
+            } else {
+                sb.append(0).append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+    public static boolean binarySearch(int[] arr, int target) {
+        int startIdx = 0;
+        int endIdx = arr.length - 1;
+
+        while (startIdx <= endIdx) {
+            int midIdx = (startIdx + endIdx) / 2;
+            int midNum = arr[midIdx];
+
+            if (midNum < target) {
+                startIdx = midIdx + 1;
+            } else if (midNum > target) {
+                endIdx = midIdx - 1;
+            } else {
+                return true; // 찾았을 경우 즉시 true 반환
+            }
+        }
+
+        return false;
     }
 }
