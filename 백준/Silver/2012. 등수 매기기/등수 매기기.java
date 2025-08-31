66a0174ebb7cc73cfe208554bd682dbc7937d829
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2025-08-31 15:32:42 +0900

    [Silver III] Title: 등수 매기기, Time: 784 ms, Memory: 54184 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        long sum = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i = 1; i <= N; i++) {
+            sum += Math.abs(arr[i - 1] - i);
+        }
+        System.out.println(sum);
+    }
+}
