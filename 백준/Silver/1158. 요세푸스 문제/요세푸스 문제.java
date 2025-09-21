fc3a2756e2295b9e7c4e140012496c93ec0b5bac
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2025-09-21 14:03:38 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 232 ms, Memory: 15068 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -5,35 +5,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder("<");
         int N = Integer.parseInt(st.nextToken()); // 숫자 총 개수
         int M = Integer.parseInt(st.nextToken()); // 시작 배수
-        int index = 1;
-        Queue<Integer> queue = new ArrayDeque<>();
 
-        //숫자를 채우고
+        Queue<Integer> queue = new ArrayDeque<>();
         for (int i = 1; i <= N; i++) {
             queue.add(i);
         }
-        //숫자가 없어 질때까자
-        while (!queue.isEmpty()) {
 
-            if (index != M) {
-                index++;
-                int arr = queue.poll();
-                queue.add(arr);
-            } else {
-                int poll = queue.poll();
-                index = 1;
-                if (queue.size() >= 1) {
-                    sb.append(poll).append(", ");
-                } else {
-                    sb.append(poll);
-                }
+        StringBuilder sb = new StringBuilder("<");
 
+        while (queue.size() > 1) {
+            for (int i = 0; i < M - 1; i++) {
+                queue.add(queue.poll());
             }
+            sb.append(queue.poll()).append(", ");
         }
-        sb.append(">");
+
+        sb.append(queue.poll()).append(">");
+
         System.out.println(sb);
     }
 }
