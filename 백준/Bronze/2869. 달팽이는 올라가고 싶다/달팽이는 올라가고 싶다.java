8935d3b7df8955a5cdaf1d57d7f0090b26f30570
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2026-03-02 22:39:08 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long V = Long.parseLong(st.nextToken());
+
+        long days = (V - A + (A - B - 1)) / (A - B) + 1;
+
+        System.out.println(days);
+
+    }
+}
