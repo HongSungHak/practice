03e356b35f99fbabc1023bec9eb03a7049484d6b
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2026-03-06 13:26:56 +0900

    [Silver IV] Title: 수 찾기, Time: 1172 ms, Memory: 59968 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,58 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
 
-        Arrays.sort(arr);
+        Map<Integer, Boolean> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            map.put(key, true);
+        }
 
         int M = Integer.parseInt(br.readLine());
+
         st = new StringTokenizer(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < M; i++) {
-            int target = Integer.parseInt(st.nextToken());
+            int key = Integer.parseInt(st.nextToken());
 
-            if (binarySearch(arr, target)) {
-                sb.append(1).append('\n');
-            } else {
-                sb.append(0).append('\n');
-            }
+            System.out.println(map.containsKey(key) ? 1 : 0);
         }
-
-        System.out.print(sb);
-    }
-
-    public static boolean binarySearch(int[] arr, int target) {
-        int startIdx = 0;
-        int endIdx = arr.length - 1;
-
-        while (startIdx <= endIdx) {
-            int midIdx = (startIdx + endIdx) / 2;
-            int midNum = arr[midIdx];
-
-            if (midNum < target) {
-                startIdx = midIdx + 1;
-            } else if (midNum > target) {
-                endIdx = midIdx - 1;
-            } else {
-                return true; // 찾았을 경우 즉시 true 반환
-            }
-        }
-
-        return false;
     }
 }
