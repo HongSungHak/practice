2a5a19a11994dcf6cafed57c45a0b81dfc921bcc
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2026-03-08 23:27:21 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int[] clothes = new int[n + 1];
+        Arrays.fill(clothes, 1);
+        for (int i : lost) clothes[i] -= 1;
+        for (int i : reserve) clothes[i] += 1;
+        
+        for (int i = 1; i < clothes.length; i++) {
+            if (clothes[i] == 0) {
+                if (i > 1 && clothes[i - 1] == 2) {
+                    clothes[i - 1]--;
+                    clothes[i]++;
+                } else if (i < n && clothes[i + 1] == 2) {
+                    clothes[i + 1]--;
+                    clothes[i]++;
+                }
+            }
+            
+        }
+        for (int i = 1; i < clothes.length; i++) {
+            if (clothes[i] >= 1) answer++;
+        }
+        return answer;
+    }
+}
