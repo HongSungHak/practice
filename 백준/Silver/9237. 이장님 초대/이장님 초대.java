5fed6020af05f6356a18be5150fbd2ac789d8b99
SUNGHAK HONG <101723087+HongSungHak@users.noreply.github.com>
2026-03-25 16:42:17 +0900

    [Silver V] Title: 이장님 초대, Time: 432 ms, Memory: 29072 KB -BaekjoonHub

---
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        int day = 0;
+        for (int i = 0; i < N; i++) {
+            day = Math.max(day, arr[i] + i + 1);
+        }
+        //다음날 초대
+        day++;
+        System.out.println(day);
+    }
+}
